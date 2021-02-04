ac360237b299318886a9fc051021b213f6a0af61
Frederik <Frederik.skarbyottosen@gmail.com>
2021-02-04 15:52:04 +0100

    Exercises

---
+++ NodeJS_B/0._Misc/1._Intro_Exercises/1.Basics.js
@@ -0,0 +1,33 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constants
+
+const firstName = "Frederik";
+const lastName = "Ottosen";
+
+// EXERCISE
+// show in the console
+// My first name is Frederik and my last name is Ottosen
+
+//Concatanation
+console.log("My first name is " + firstName +" and my last name is " + lastName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2020";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2021
+// You cannot touch line 1 or 2
+
+//Example Solution 1
+console.log(parseInt(year) + number);
+
+//Example Solution 2
+const newyear = parseInt(year)+number;
+console.log(newyear);
+
+// --------------------------------------
